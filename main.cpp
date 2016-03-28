18e3b9469c5d2d9f5c485d5c3f7e4fd9fa968d11
Arho Huttunen <arho.huttunen@iki.fi>
2016-03-28 12:38:01 +0300

    Hide the cursor.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
     app.setApplicationName("fractal");
+    app.setOverrideCursor(Qt::BlankCursor);
 
     FractalWidget widget;
     widget.resize(800, 480);
